5b1f95461eb487c17a9f4283c24bdece727a652c
fahad-poran <55590627+fahad-poran@users.noreply.github.com>
2021-03-02 21:52:19 +0600

    update Email
    email

--- T3/view/pagetwo.php
+++ T3/view/pagetwo.php
@@ -23,7 +23,7 @@ $conobj=$connection->OpenCon();
 $userQuery=$connection->ShowAll($conobj,"student");
 
 if ($userQuery->num_rows > 0) {
-    echo "<table><tr><th>Name</th><th>Email</th><th>Address</th><th>Profession</th></tr>";
+    echo "<table><tr><th>Name</th><th>Email</th><th>Department</th><th>Location</th></tr>";
     // output data of each row
 
     // while($row = $userQuery->fetch_assoc()) {
@@ -31,7 +31,7 @@ if ($userQuery->num_rows > 0) {
     // }
     //making change 
      while($row = $userQuery->fetch_assoc()) {
-      echo "<tr><td>".$row["age"]."</td><td>".$row["age"]."</td><td>".$row["department"]."</td><td>".$row["location"]."</td></tr>";
+      echo "<tr><td>".$row["age"]."</td><td>".$row["Email"]."</td><td>".$row["department"]."</td><td>".$row["location"]."</td></tr>";
     }
 
     echo "</table>";
